1e18b644febfaa7b7b2beb1ce7ad34ac8fe97ddb
Cristian <76985685+Dr-Cristian@users.noreply.github.com>
2024-05-03 10:31:20 -0300

    Base de datos. 3/05/2024

---
+++ tesina_grupo6.sql
@@ -0,0 +1,425 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 02-05-2024 a las 15:00:35
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `tesina_grupo6`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `det_pago`
+--
+
+CREATE TABLE `det_pago` (
+  `id` int(11) NOT NULL,
+  `id_pago` int(11) NOT NULL,
+  `monto` int(10) NOT NULL,
+  `id_metodop` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `especialidad`
+--
+
+CREATE TABLE `especialidad` (
+  `id` int(11) NOT NULL,
+  `tipo` varchar(75) NOT NULL,
+  `descrip` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `especialidad`
+--
+
+INSERT INTO `especialidad` (`id`, `tipo`, `descrip`) VALUES
+(1, 'Anatomía Patológica', ''),
+(2, 'Anestesiología', ''),
+(3, 'Cardiología', ''),
+(4, 'Cirugía Cardiovascular', ''),
+(5, 'Cirugía General', ''),
+(6, 'Cirugía Pediátrica', ''),
+(7, 'Cirugía Plástica y Reparadora', ''),
+(8, 'Cirugía Torácica', ''),
+(9, 'Cirugía Vascular Periférica', ''),
+(10, 'Clínica Médica', ''),
+(11, 'Dermatología', ''),
+(12, 'Endocrinología', ''),
+(13, 'Farmacología', ''),
+(14, 'Gastroenterología', ''),
+(15, 'Genética Médica', ''),
+(16, 'Geriatría', ''),
+(17, 'Ginecología y Obstetricia', ''),
+(18, 'Hematología', ''),
+(19, 'Infectología', ''),
+(20, 'Medicina del Trabajo', ''),
+(21, 'Medicina Familiar y Comun', ''),
+(22, 'Fisiatria(Medicina Física y Rehabilitacion)', ''),
+(23, 'Medicina Intensiva', ''),
+(24, 'Medicina Interna', ''),
+(25, 'Medicina Legal y Forense', ''),
+(26, 'Medicina Nuclear', ''),
+(27, 'Medicina Preventiva y Salud', ''),
+(28, 'Nefrología', ''),
+(29, 'Neumonología', ''),
+(30, 'Neurocirugía', ''),
+(31, 'Neurología', ''),
+(32, 'Nutrición', ''),
+(33, 'Oftalmología', ''),
+(34, 'Oncología Médica', ''),
+(35, 'Oncología Radioterápica', ''),
+(36, 'Ortopedia y Traumatología', ''),
+(37, 'Otorrinolaringología', ''),
+(38, 'Pediatría', ''),
+(39, 'Psiquiatría', ''),
+(40, 'Radiología', ''),
+(41, 'Reumatología', ''),
+(42, 'Toxicología Médica', ''),
+(43, 'Urología', '');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `horario_medico`
+--
+
+CREATE TABLE `horario_medico` (
+  `id` int(11) NOT NULL,
+  `dia_sem` varchar(20) NOT NULL,
+  `hora_inicio` time NOT NULL,
+  `hora_final` time NOT NULL,
+  `id_medico` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `medico`
+--
+
+CREATE TABLE `medico` (
+  `id` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_especialidad` int(11) NOT NULL,
+  `email` varchar(75) NOT NULL,
+  `telefono` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `met_pago`
+--
+
+CREATE TABLE `met_pago` (
+  `id` int(11) NOT NULL,
+  `metodo` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `obra_social`
+--
+
+CREATE TABLE `obra_social` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `paciente`
+--
+
+CREATE TABLE `paciente` (
+  `ID_Paciente` int(11) UNSIGNED NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `Nombres` varchar(100) NOT NULL,
+  `Apellidos` varchar(100) NOT NULL,
+  `Peso` float NOT NULL,
+  `Altura` float NOT NULL,
+  `Edad` int(11) NOT NULL,
+  `Telefono` varchar(9) NOT NULL,
+  `Alergias` varchar(200) NOT NULL,
+  `Enfermedades` varchar(200) NOT NULL,
+  `id_tipo_sangre` int(11) NOT NULL,
+  `Estado` tinyint(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pago`
+--
+
+CREATE TABLE `pago` (
+  `id` int(11) NOT NULL,
+  `fecha_pago` date NOT NULL,
+  `id_paciente` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `qr_code`
+--
+
+CREATE TABLE `qr_code` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `ruta_image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rol`
+--
+
+CREATE TABLE `rol` (
+  `id` int(11) NOT NULL,
+  `rol` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `rol`
+--
+
+INSERT INTO `rol` (`id`, `rol`) VALUES
+(1, 'Usuario'),
+(2, 'Admin'),
+(3, 'Recepcion'),
+(4, 'Medico');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_sanguineo`
+--
+
+CREATE TABLE `tipo_sanguineo` (
+  `id` int(11) NOT NULL,
+  `tipo` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `turno`
+--
+
+CREATE TABLE `turno` (
+  `id` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_medico` int(11) NOT NULL,
+  `id_qrcode` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id_usuario` int(11) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `email` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `especialidad`
+--
+ALTER TABLE `especialidad`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `horario_medico`
+--
+ALTER TABLE `horario_medico`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `medico`
+--
+ALTER TABLE `medico`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `met_pago`
+--
+ALTER TABLE `met_pago`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `obra_social`
+--
+ALTER TABLE `obra_social`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `paciente`
+--
+ALTER TABLE `paciente`
+  ADD UNIQUE KEY `ID_Paciente` (`ID_Paciente`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `pago`
+--
+ALTER TABLE `pago`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `qr_code`
+--
+ALTER TABLE `qr_code`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `rol`
+--
+ALTER TABLE `rol`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `tipo_sanguineo`
+--
+ALTER TABLE `tipo_sanguineo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `turno`
+--
+ALTER TABLE `turno`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_qrcode` (`id_qrcode`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `especialidad`
+--
+ALTER TABLE `especialidad`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+
+--
+-- AUTO_INCREMENT de la tabla `horario_medico`
+--
+ALTER TABLE `horario_medico`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `medico`
+--
+ALTER TABLE `medico`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `met_pago`
+--
+ALTER TABLE `met_pago`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `obra_social`
+--
+ALTER TABLE `obra_social`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `paciente`
+--
+ALTER TABLE `paciente`
+  MODIFY `ID_Paciente` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `pago`
+--
+ALTER TABLE `pago`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `qr_code`
+--
+ALTER TABLE `qr_code`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `rol`
+--
+ALTER TABLE `rol`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `tipo_sanguineo`
+--
+ALTER TABLE `tipo_sanguineo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `turno`
+--
+ALTER TABLE `turno`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `paciente`
+--
+ALTER TABLE `paciente`
+  ADD CONSTRAINT `paciente_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`);
+
+--
+-- Filtros para la tabla `turno`
+--
+ALTER TABLE `turno`
+  ADD CONSTRAINT `turno_ibfk_1` FOREIGN KEY (`id_qrcode`) REFERENCES `qr_code` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
